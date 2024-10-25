871dbc14bef17112b730852edcc393a0f4008801
Gabriel <gabs07099@gmail.com>
2024-10-25 15:04:39 -0300

    Envio Projeto

---
+++ TempMotoWeb/Controllers/MedicoesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TempMotoWeb.Data;
+using TempMotoWeb.Models;
+
+
+namespace TempMotoWeb.Controllers
+{
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MedicoesController : ControllerBase
+    {
+        private readonly AquaContext _context;
+
+        public MedicoesController(AquaContext context)
+        {
+            _context = context;
+        }
+
+        // POST api/<ApiController>
+        [HttpPost]
+        public async Task<IResult> Post(Medicao medicao)
+        {
+            try
+            {
+                _context.Medicao.Add(medicao);
+                await _context.SaveChangesAsync();
+
+                return Results.Created($"/medicao/{medicao.Id}", medicao);
+            }catch(Exception e)
+            {
+                return Results.Problem(e.Message+e.StackTrace);
+            }
+        }
+
+        // DELETE api/<ApiController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
